bef9dd0d4ca6d7fe672d373b35c5dc93dad3e86c
michael-shambel <Michaelshambel10@gmail.com>
2024-07-05 01:26:07 +0300

    task o

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,14 +10,14 @@ def canUnlockAll(boxes):
     accepts: lists contain keys and 
     return: boolean true if can be unlocked else false
     """
-    unlocked = [False] * len(boxes)
-    unlocked[0] = True
+    locked = [False] * len(boxes)
+    locked[0] = True
     keys = [0]
 
     while keys:
         every_key = keys.pop()
         for key in boxes[every_key]:
-            if key < len(boxes) and not unlocked[key]:
-                unlocked[key] = True
+            if key < len(boxes) and not locked[key]:
+                locked[key] = True
                 keys.append(key)
-    return all(unlocked)
+    return all(locked)
