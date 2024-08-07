c81b10a37e2c7dfd6ece27a7351fabef4640c060
michael-shambel <Michaelshambel10@gmail.com>
2024-08-07 14:56:22 +0300

    task o

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,10 +1,22 @@
 #!/usr/bin/node
+/**
+ * this code will help to point of all character
+ * in the specified movie id
+ * the movie id is passed as an argument
+ * the code will return the name of all characters
+ * */
+// this line will help to import the request module
 const request = require('request');
 
+// this line will help to get the movie id from the command line
 const movieId = process.argv[2];
+// this line will help to get the url of the movie
 const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+// this function will help to get the movie info by passing the url
 const getMoviesInfo = (url) => {
+  // this line will help to return a promise to get the data
   return new Promise((resolve, reject) => {
+    // this line will help to get the data from the url
     request(url, (error, Response, body) => {
       if (error) {
         reject(error);
@@ -15,8 +27,11 @@ const getMoviesInfo = (url) => {
   });
 };
 
+// this function will help to get the character name by passing the character url
 const getCharacter = (characterurl) => {
+  // this line will help to return a promise to get the character name
   return new Promise((resolve, reject) => {
+    // this line will help to get the data from the character url
     request(characterurl, (error, Response, body) => {
       if (error) {
         reject(error);
@@ -26,7 +41,14 @@ const getCharacter = (characterurl) => {
     });
   });
 };
-
+// this function will help to get the character name by passing the url
+/**
+ * i used async and await to get the character name
+ * by passing the url of the movie then i get the data
+ * from the movie and then i get the character url
+ * then i get the character name by passing the character url
+ * then i print the character name
+ */
 async function getCharacterName (url) {
   try {
     const data = await getMoviesInfo(url);
