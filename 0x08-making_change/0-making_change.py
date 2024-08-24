02bd6a492b7e5f53058b40be43a4473aace5e7e0
michael-shambel <Michaelshambel10@gmail.com>
2024-08-24 12:01:30 +0300

    task 0 up2

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,16 +11,16 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    
+
     dynamicArray = [float('inf')] * (total + 1)
     dynamicArray[0] = 0
 
     for coin in coins:
         for i in range(coin, total + 1):
             if dynamicArray[i - 1] != float('inf'):
-                dynamicArray[i] = min(dynamicArray[i], dynamicArray[i - coin] + 1)
+                dynamicArray[i] = min(
+                    dynamicArray[i], dynamicArray[i - coin] + 1)
     if dynamicArray[total] != float('inf'):
         return dynamicArray[total]
     else:
         return -1
-   
